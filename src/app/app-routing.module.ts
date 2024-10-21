db2c3a6a2c11a175905042fd2cf618d324fd264c
Lybnih <132515156+Lybnih@users.noreply.github.com>
2024-10-21 12:52:15 +0000

    Adição AddPost

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FeedComponent } from './feed/feed.component';
+import { CurriculoComponent } from './curriculo/curriculo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'feed', pathMatch: 'full' },
+  { path: 'feed', component: FeedComponent },
+  { path: 'curriculo', component: CurriculoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
