75f3f022741a932455797695b669c6c157714143
Lybnih <132515156+Lybnih@users.noreply.github.com>
2024-10-23 00:38:46 +0000

    lybnih

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,20 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { ServerService } from '../server.service';
+import { Post } from '../post.model';
+
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.css']
 })
-export class FeedComponent implements OnInit {
-  posts: any[] = []; 
+export class FeedComponent implements OnInit{
+  posts: Post[] = [
+    {
+      author: 'Kawan Xavier',
+      title: 'Desenvolvedor Back-End',
+      content: 'BR: O Brasil tem hoje 10 milhões de pessoas surdas. Imagine aprender Libras ou usar uma ferramenta para conversar com elas...',
+      image: 'assets/post-image.jpg' // Substitua pela URL da imagem
+    },
+    {
+      author: 'Paulo Castellani',
+      title: 'Vagas de TI',
+      content: 'Atuando como Job Hunter, estou aqui para oferecer suporte personalizado em sua jornada rumo ao próximo passo em sua carreira. Possuo experiência auxiliando profissionais a alcançarem seus objetivos, estando comprometido em...',
+      image: 'null'
+    }
+  ];
 
-  constructor(private serverService: ServerService) { }
-
-  ngOnInit(): void {
-    this.serverService.getPosts().subscribe((data) => {
-      this.posts = data;  
-      console.log(data);
-    });
-  }
 }
